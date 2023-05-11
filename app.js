5554bd114f355c524cbc0285ecc72e3d1e2f6567
Josiah Humber <humberj@flip1.engr.oregonstate.edu>
2023-05-10 20:10:13 -0700

    create app.js

---
+++ app.js
@@ -0,0 +1,27 @@
+/*
+    SETUP for a simple webapp
+*/
+// Express
+var fs      = require('fs')
+var express = require('express');   // We are using the express library for the web server
+var exphb   = require('express-handlebars');
+var app     = express();            // We need to instantiate an express object to interact with the server in our code
+PORT        = process.env.PORT || 2990;                 // Set a port number at the top so it's easy to change in the future
+// Database
+var db = require('./db-connector')
+/*
+    ROUTES
+*/
+app.get('/', function(req, res)
+    {
+       res.status(200).render('mainPage')
+});
+
+app.get('*', function (req, res) {
+	res.status(404).render('404');
+});
+
+app.listen(port, function (err) {
+	if(err) throw err;
+	console.log('== Server is listening on port', port);
+});
