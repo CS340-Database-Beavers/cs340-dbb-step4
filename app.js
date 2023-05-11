4a866aaa6fed34dc900a1a9630b6e7417485381d
Josiah Humber <humberj@flip1.engr.oregonstate.edu>
2023-05-10 20:28:33 -0700

    added default hbs view to app.js

--- app.js
+++ app.js
@@ -4,11 +4,16 @@
 // Express
 var fs      = require('fs')
 var express = require('express');   // We are using the express library for the web server
-var exphb   = require('express-handlebars');
+var exphbs  = require('express-handlebars');
 var app     = express();            // We need to instantiate an express object to interact with the server in our code
-PORT        = process.env.PORT || 2990;                 // Set a port number at the top so it's easy to change in the future
+PORT        = process.env.PORT || 7518;                 // Set a port number at the top so it's easy to change in the future
 // Database
 var db = require('./db-connector')
+//Handlebars
+app.engine('handlebars', exphbs.engine({ defaultLayout: 'main' }));
+app.set('view engine', 'handlebars');
+
+app.use(express.json());
 /*
     ROUTES
 */
