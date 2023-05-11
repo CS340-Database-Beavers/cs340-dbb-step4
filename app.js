89b225ff90d2b677068149bc31186822d0671085
Josiah Humber <humberj@flip1.engr.oregonstate.edu>
2023-05-10 20:40:52 -0700

    changed default port to 4220 (dbb to numbers)

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ var fs      = require('fs')
 var express = require('express');   // We are using the express library for the web server
 var exphbs  = require('express-handlebars');
 var app     = express();            // We need to instantiate an express object to interact with the server in our code
-PORT        = process.env.PORT || 7518;                 // Set a port number at the top so it's easy to change in the future
+PORT        = process.env.PORT || 4220;                 // Set a port number at the top so it's easy to change in the future
 // Database
 var db = require('./db-connector')
 //Handlebars
