090c7a5457d9f1982c147794674b27d485203c55
nairnhoc <nairnhoc@oregonstate.edu>
2023-05-11 19:11:21 -0700

    changed SQL statements to have variables (no longer syntactically correct)

--- DML.sql
+++ DML.sql
@@ -1,11 +1,13 @@
+-- all variable names are surrounded by two colons (eg, %%var_name%%)
+
 -------------------------- Role Table Queries --------------------------
 -- Adding a new role to the table
 INSERT INTO roles VALUES
-(DEFAULT,"name");
+(DEFAULT,%%role_name%%);
 
 -- Changing an existing role
-UPDATE roles SET role_name="new_name"
-WHERE role_id=1;
+UPDATE roles SET role_name=%%role_name%%
+WHERE role_id=%%role_id%%;
 
 -- Deleting a role might cause problems since other data depends on it
 -- even if nobody uses the role now, that may not be the case in the future...
@@ -18,7 +20,13 @@ WHERE role_id=1;
 -------------------------- Employee Table Queries --------------------------
 -- Adding a new employee to the DB
 INSERT INTO employees VALUES
-(DEFAULT, '2023-5-10', "Tommy Wiseo", 11, 1, "420 Godknowswhere St", '1970-5-10');
+(DEFAULT, 
+%%hire_date%%, 
+%%full_name%%,
+%%role_id%%,
+1, 
+%%address%%, 
+%%birthdate%%);
 
 -- We shouldn't be taking employees out of the DB, in case we need to
 -- refer back to them in the future for any reason
@@ -27,15 +35,21 @@ INSERT INTO employees VALUES
 -- We should, however, mark employees as "unactive" if they leave the
 -- company
 UPDATE employees SET is_active=0 
-WHERE employee_id=5;
+WHERE employee_id=%%employee_id%%;
 
 -- or as active if they re-join the company
 UPDATE employees SET is_active=1 
-WHERE employee_id=3;
+WHERE %%employee_id%%=3;
 
 -- We should also allow our admin to modify the other aspects of an employee's data
-UPDATE employees SET hire_date='1111-1-1', name="none", role=11, is_active=0, address="none", birthdate="1111-1-1"
-WHERE employee_id=1;
+UPDATE employees SET 
+hire_date=%%hire_date%%, 
+name=%%new_name%%, 
+role=%%role_id%%, 
+is_active=%%is_active%%, 
+address=%%address%%, 
+birthdate=%%birthdate%%
+WHERE employee_id=%%employee_id%%;
 
 -- SELECT * FROM employees;
 
@@ -44,10 +58,10 @@ WHERE employee_id=1;
 -------------------------- Salaries Table Queries --------------------------
 -- Adding a new salary to the table
 INSERT INTO salaries VALUES
-(DEFAULT,'1111-1-1',0.0,1);
+(DEFAULT,%%effective_date%%, %%ammount%%, %%employee_id%%);
 
 -- We don't want to have ability to UPDATE salaries; this would make
--- the admin unable to track an employees' salary history
+-- the admin unable to track an employee's true salary history
 -- NO UPDATE
 
 -- Under the same logic, we don't want to be able to DELETE salaries,
@@ -61,7 +75,7 @@ SELECT * FROM salaries;
 -------------------------- Projects Table Queries --------------------------
 -- Adding a new project to the table
 INSERT INTO projects VALUES
-(DEFAULT,1,0,'1111-1-1','1111-1-1',"none");
+(DEFAULT, 1, 0, %%deadline%%, %%start_date%%, %%project_name%%);
 
 -- We want to be able to refer back to previous projects for analytics,
 -- so we shouldn't DELETE projects
@@ -69,39 +83,48 @@ INSERT INTO projects VALUES
 
 -- Instead, as with employees, we should mark projects as inactive
 UPDATE projects SET is_ongoing=0
-WHERE project_id=1;
+WHERE project_id=%%project_id%%;
 
 -- Or active
 UPDATE projects SET is_ongoing=1
-WHERE project_id=3;
+WHERE project_id=%%project_id%%;
 
 -- Similarly, we should allow the other attributes of a project
 -- to be edited by an admin
-UPDATE projects SET project_name="something", percent_completed=0, deadline='1111-1-1', start_date='1111-1-1'
-WHERE project_id=2;
+UPDATE projects SET project_name=%%project_name%%, 
+percent_completed=%%percent_completed%%, 
+deadline=%%deadline%%, 
+start_date=%%start_date%%
+WHERE project_id=%%project_id%%;
 
 -- For ease of use, there should be specific queries to edit parts of a
 -- project that may change often, such as the deadline or % completed
-UPDATE projects SET deadline='2222-2-2'
-WHERE project_id=2;
+UPDATE projects SET deadline=%%deadline%%
+WHERE project_id=%%project_id%%;
 
-UPDATE projects SET percent_completed=100
-WHERE project_id=2;
+UPDATE projects SET percent_completed=%%percent_completed%%
+WHERE project_id=%%project_id%%;
 
 
 -------------------------- Employees_Projects Table Queries --------------------------
 -- Adding an entry
 INSERT INTO employees_projects VALUES
-(1,1,'1111-1-1',24);
+(%%employee_id%%, %%project_id%%, %%date_of_work%%, %%number_hours%%);
 
 -- In the case where an admin finds incorrect information entered by
 -- an employee (eg, wrong project listed, wrong date of work), we
 -- should give the admin the ability to correct the mistake
-UPDATE employees_projects SET project_id=1, date_of_work='1111-1-1', number_hours=0
-WHERE employee_id=1 AND project_id=2 AND date_of_work='1111-1-1';
+UPDATE employees_projects SET 
+project_id=%%correct_project_id%%, 
+date_of_work=%%correct_date_of_work%%, 
+number_hours=%%correct_number_hours%%
+WHERE employee_id=%%target_employee_id%% 
+AND project_id=%%target_project_id%% 
+AND date_of_work=%%target_day_of_work%%;
 
 -- In the case where an admin finds falsified information (eg, an
 -- employee listing that they worked on a day when they didn't),
 -- the admin should have the ability to DELETE that information
 DELETE FROM employees_projects
-WHERE employee_id=1 AND project_id=1 AND date_of_work='1111-1-1';
+WHERE employee_id=%%bad_employee%% 
+AND date_of_work=%%target_day_of_work%%;
