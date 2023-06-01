bfd0ac87ea874f2acb1239c136428882530dbd7b
CupofJoe <josiah.humber@gmail.com>
2023-06-01 15:08:58 -0700

    fks are now dropdowns for inputs

--- app.js
+++ app.js
@@ -127,9 +127,28 @@ async function getTableFromFK(fkInfo) {
         });
       });
       // console.log(results);
-      retVal[fkInfo[i].COLUMN_NAME] = results.find((column) =>
-        column.Field.includes("name")
-      ); // Resolve the promise with the query results
+      retVal[fkInfo[i].COLUMN_NAME] = {
+        name: results.find((column) => column.Field.includes("name")),
+        id: results.find((column) => column.Field.includes("id")),
+      }; // Resolve the promise with the query results
+    } catch (error) {
+      console.log(error);
+      throw error; // Re-throw the error to be caught in the calling code
+    }
+    var columnsQ = "SELECT * FROM " + fkInfo[i].REFERENCED_TABLE_NAME + ";";
+    try {
+      const results = await new Promise((resolve, reject) => {
+        db.pool.query(columnsQ, function (err, results, fields) {
+          if (err) {
+            reject(err);
+          } else {
+            resolve(results);
+          }
+        });
+      });
+      // console.log(results);
+      retVal[fkInfo[i].COLUMN_NAME]["select"] = results;
+      console.log(retVal);
     } catch (error) {
       console.log(error);
       throw error; // Re-throw the error to be caught in the calling code
